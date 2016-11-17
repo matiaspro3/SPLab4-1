eda732d4a586f28aefcd3d45151459cf6f287513
matiaspro3 <matias9387@hotmail.com>
2016-11-17 15:02:28 -0300

    Filtros personalizados

--- js/controladorUsuarioGr.js
+++ js/controladorUsuarioGr.js
@@ -1,13 +1,31 @@
 angular.module('abmapp.filtrosUsuarioGrilla', []) 
   app.filter('tipo', function () {
-/*   var perfil = {
+   var perfil = {
       'admin': 'Administrador',
       'vend': 'Vendedor',
       'comp': 'Comprador'
     }
-  */  return function (input) {
+   return function (input) {
         if (!input)
         return '';
-    //  return perfil[input];
+    return perfil[input];
+    }
+
+});
+app.filter('sexo', function () {
+   var sex = {
+      'f': 'Femenino',
+      'm': 'Masculino'
+      
+    }
+   return function (input) {
+        if (!input)
+        return '';
+    return sex[input];
     };
+
+
+
+
+
   });
